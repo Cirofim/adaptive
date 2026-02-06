13db45ee1626ba3075c22dba3254bea68f3ec1ff
Cirofim <cirofim8767@gmail.com>
2026-02-07 00:35:25 +0300

    moduleScss

--- adaptive/src/App.vue
+++ adaptive/src/App.vue
@@ -1,68 +1,16 @@
-<script>
+<script setup>
+import logo from "./components/logo.vue";
+import navigation from "./components/navigation.vue";
 </script>
-
-
 
 <template>
   <header>
-    <div class="header_container">
-      <div class="wrapper_logo-burger">
-        <div class="logo">LOGO</div>
-        <img class="burger_button" src="./assets/icons/burger.svg" alt="" />
-      </div>
-      <div class="wrapper_logo">
-        <div class="logo_text">Искать жильё</div>
-        <div class="logo_text">Блог</div>
-        <div class="logo_text">Частые вопросы</div>
-        <img
-          class="icon_logo"
-          width="24"
-          height="24"
-          src="@/assets/icons/bell.svg"
-          alt="bell"
-        />
-        <img
-          class="icon_logo"
-          width="24"
-          height="24"
-          src="@/assets/icons/like.svg"
-          alt="like"
-        />
-        <div class="logo_text big_text">Мой аккаунт</div>
-      </div>
-    </div>
-
-    <div class="wrapper_bottom">
-      <div class="wrapper_navigation">
-        <a href="" class="wrapper_link-navigation">
-          <div class="navigation_text padding-text--left">
-            Посуточная аренда
-          </div>
-        </a>
-        <div class="navigation_text trait_text">/</div>
-        <a href="" class="wrapper_link-navigation">
-          <div class="navigation_text">Барановичи, Брестская область</div>
-        </a>
-        <div class="navigation_text trait_text">/</div>
-        <a href="" class="wrapper_link-navigation">
-          <div class="navigation_text">70 - 150 BYN</div>
-        </a>
-        <div class="navigation_text trait_text">/</div>
-        <a href="" class="wrapper_link-navigation">
-          <div class="navigation_text">27.09.25 - 30.09.25</div>
-        </a>
-      </div>
-      <div class="wrapper_navigation-bottom">
-        <div class="navigation_text-pn ">Сохранить вариант</div>
-        <img class="grey_like" src="@/assets/icons/grey_like.svg" alt="" />
-      </div>
-    </div>
+    <logo />
+    <navigation />
   </header>
 </template>
 
-<style lang="scss" scoped>
-@import url(./assets/blocks/logo.scss);
+<style lang="scss">
 @import url(./assets/global/style.scss);
-@import url(./assets/blocks/navigation.scss);
 
 </style>
---
+++ adaptive/src/components/navigation.vue
@@ -0,0 +1,108 @@
+<script></script>
+
+<template>
+  <div :class="$style.bottomWr">
+    <div :class="$style.navigationWr">
+      <a href="" :class="$style.linkWr">
+        <div :class="$style.textNav">Посуточная аренда</div>
+      </a>
+      <div :class="$style.trait">/</div>
+      <a href="" :class="$style.linkWr">
+        <div :class="$style.textNav">Барановичи, Брестская область</div>
+      </a>
+      <div :class="$style.trait">/</div>
+      <a href="" :class="$style.linkWr">
+        <div :class="$style.textNav">70 - 150 BYN</div>
+      </a>
+      <div :class="$style.trait">/</div>
+      <a href="" :class="$style.linkWr">
+        <div :class="$style.textNav">27.09.25 - 30.09.25</div>
+      </a>
+    </div>
+    <div :class="$style.wrapperRight">
+      <div :class="$style.textPn">Сохранить вариант</div>
+      <img :class="$style.grey_like" src="@/assets/icons/grey_like.svg" alt="" />
+    </div>
+  </div>
+</template>
+
+<style module lang="scss">
+
+.bottomWr {
+  max-width: 64rem;
+  margin: 0 auto;
+  padding: 0 0 0 1.5rem;
+  display: flex;
+  gap: 0.75rem;
+
+  @media (max-width: 1024px) {
+    flex-wrap: wrap;
+    padding: 0;
+  }
+
+  .navigationWr {
+    display: flex;
+
+    .linkWr {
+      display: flex;
+
+      .textNav {
+        font-size: 1rem;
+        padding: 0.375rem 0.875rem 0.375rem 0.875rem;
+        white-space: nowrap;
+        color: $grey-txt;
+
+        .textNav {
+          padding: 0.375rem 0.625rem 0.375rem 0;
+        }
+
+        &:hover {
+          color: $blue-txt;
+          cursor: pointer;
+          transition: color 0.3s ease-in-out;
+        }
+
+        @media (max-width: 830px) {
+          padding: 0.375rem 0.875rem 0.375rem 0;
+        }
+      }
+    }
+
+    @media (max-width: 768px) {
+      flex-wrap: wrap;
+    }
+
+    .trait {
+      
+      padding: 0.375rem 0;
+      color: $grey-txt;
+
+      @media (max-width: 830px) {
+        padding: 0.375rem 0.875rem 0.375rem 0;
+      }
+    }
+  }
+
+  .wrapperRight {
+    display: flex;
+    gap: 8px;
+    padding: 0.375rem 0.875rem;
+    cursor: pointer;
+    color: $grey-txt;
+
+    .textPn {
+      padding: 0;
+    }
+
+    @media (max-width: 830px) {
+      padding: 0.375rem 0.875rem 0.375rem 0;
+    }
+
+    &:hover {
+      color: $blue-txt;
+      transition: color 0.3s ease-in-out;
+    }
+  }
+}
+
+</style>
